567f9c4e46aea89e7a39a1249a156eb64bca67e5
brolny <brolny@gmail.com>
2026-05-28 18:59:35 +0400

    Add files via upload

---
+++ js/ogg26.js
@@ -0,0 +1,9 @@
+var u=[
+['քսանվեց','twenty-six','kʰəsɑnˈveʦʰ'],
+['հարազատներ','oneˈs people','hɑɾɑzɑtneˈɾ'],
+['բոլոր','all','boloˈɾ'],
+['ամեն','every','ɑˈmen'],
+['որևէ','any','ˈvoɾeve'],
+['ատել','to hate','ɑˈtel'],
+['թշնամի','enemy','tʰəʃnɑˈmi'],
+['մորաքույր','maternal aunt','moɾɑˈkʰujɾ']];
